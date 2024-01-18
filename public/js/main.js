b7f36236b718b1cde19cfcad1ac8034fb3c3f681
Melina1996 <melina.schlachter@gmx.de>
2024-01-18 20:53:06 +0100

    added first js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,20 @@
 
-let valueNumber = document.querySelector("input[type=number]")
+//select the input number field
+let valueNumber = document.querySelectorAll("input[type=number]")
 
 console.log(valueNumber)
+
+//my password to be generated is equal to the number value
+let passwordLength = valueNumber.value
+
+let myFirstCheckbox = document.querySelector("#uppercase")
+
+myFirstCheckbox.addEventListener("click",()=>{
+    console.log(myFirstCheckbox.checked)
+})
+
+let mySecondCheckbox = document.querySelector("#lowercase")
+
+let myThirdCheckbox = document.querySelector("#numbers")
+
+let myFourthCheckbox = document.querySelector("#symbols")
