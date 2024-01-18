87edaed2159085df9d9dda455ca1f8b8ec73e716
Melina1996 <melina.schlachter@gmx.de>
2024-01-18 19:43:07 +0100

    added folder structure

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+//variables for each of my h2
+
+let firstH2 = document.querySelector("#firstH2")
+
+let secondH2 = document.querySelector("#secondH2")
+
+let thirdH2 = document.querySelector("#thirdH2")
+
+// let allSocialMedia = [firstH2,secondH2,thirdH2]
+
+
+// let allSocialMedia = document.querySelectorAll(".each-logo span")
+
+// allSocialMedia.forEach(element => {
+//   element.innerText = "0";
+
+//   function counting(){
+//     let maxCount = +element.getAttribute("data-target")
+//     let myH2Count = +element.innerText
+
+//     let increment = maxCount/200 //will increment by 200er-steps
+
+//     if(myH2Count < maxCount){
+//       element.innerText = Math.round(myH2Count + increment);
+//       setTimeout(counting,1)
+//     } else {
+//       element.innerText = maxCount
+//     }
+//   }
+//   counting()
+//   })
+
+
+
